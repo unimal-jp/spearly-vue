79a94b5c03e1aa7fb7bf1b3cb0a4438dd3aa7072
Kenty Akoarum <kamome-black@akoarum.net>
2023-08-08 16:58:33 +0900

    upgrade jest vertion to 29

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,14 @@ module.exports = {
   roots: ['<rootDir>/src'],
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.test.json',
-    },
-  },
   moduleFileExtensions: ['js', 'ts', 'vue', 'json', 'node'],
   transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: './tsconfig.test.json'
+    }],
   },
+  transformIgnorePatterns: ['/node_modules/(?!(axios)/)'],
   testMatch: [
     '<rootDir>/src/**/*.spec.{js,ts}',
   ],
