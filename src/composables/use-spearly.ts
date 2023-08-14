91d7562b33976bd4e0807e71d7d87bcd7eb124bd
Kenty Akoarum <kamome-black@akoarum.net>
2023-08-14 12:23:58 +0900

    add analytics functions to useSpearly

--- src/composables/use-spearly.ts
+++ src/composables/use-spearly.ts
@@ -1,33 +1,74 @@
-import { getCurrentInstance } from 'vue'
-import { SpearlyApiClient } from '@spearly/sdk-js'
+import { ref, getCurrentInstance } from 'vue'
+import { SpearlyApiClient, SpearlyAnalytics } from '@spearly/sdk-js'
 import type { GetContentParams, GetParams } from '@spearly/sdk-js'
 
 export const useSpearly = () => {
   const app = getCurrentInstance()
   const apiClient = new SpearlyApiClient(app?.appContext.config.globalProperties.apiKey)
+  const analytics = new SpearlyAnalytics()
+
+  const contentId = ref<string>()
+  const contentPatternName = ref<'a' | 'b'>()
+  const formVersion = ref<number>()
 
   const getContentList = async (contentTypeId: string, params: GetParams) => {
     return await apiClient.getList(contentTypeId, params)
   }
 
-  const getContent = async (contentId: string, params?: GetContentParams) => {
-    return await apiClient.getContent(contentId, params)
+  const getContent = async (id: string, params?: GetContentParams) => {
+    const response = await apiClient.getContent(id, params)
+    contentId.value = response.id
+    contentPatternName.value = response.attributes.patternName
+    return response
   }
 
-  const getContentPreview = async (contentId: string, previewToken: string) => {
-    return await apiClient.getContentPreview(contentId, previewToken)
+  const getContentPreview = async (id: string, previewToken: string) => {
+    return await apiClient.getContentPreview(id, previewToken)
   }
 
   const getFormLatest = async (publicUid: string) => {
-    return await apiClient.getFormLatest(publicUid)
+    const response = await apiClient.getFormLatest(publicUid)
+    formVersion.value = response.id
+    return response
   }
 
   const postFormAnswer = async (
-    formVersionId: number,
-    fields: { [key: string]: unknown } & { _spearly_gotcha: string }
+    fields: { [key: string]: unknown } & { _spearly_gotcha: string },
+    formVersionId?: number
   ) => {
-    return await apiClient.postFormAnswers(formVersionId, fields)
+    if (!formVersionId && !formVersion.value) throw new Error('version ID is not specified')
+    return await apiClient.postFormAnswers(formVersionId || formVersion.value!, fields)
   }
 
-  return { getContentList, getContent, getContentPreview, getFormLatest, postFormAnswer }
+  const pageView = async (id?: string, patternName?: 'a' | 'b') => {
+    const submitData = {
+      contentId: id || contentId.value,
+      patternName: patternName || contentPatternName.value,
+    }
+
+    if (!submitData.contentId) throw new Error('contentId is not defined.')
+    if (!submitData.patternName) throw new Error('patternName is not defined.')
+
+    await analytics.pageView({
+      contentId: submitData.contentId,
+      patternName: submitData.patternName,
+    })
+  }
+
+  const conversion = async (id?: string, patternName?: 'a' | 'b') => {
+    const submitData = {
+      contentId: id || contentId.value,
+      patternName: patternName || contentPatternName.value,
+    }
+
+    if (!submitData.contentId) throw new Error('contentId is not defined.')
+    if (!submitData.patternName) throw new Error('patternName is not defined.')
+
+    await analytics.conversion({
+      contentId: submitData.contentId,
+      patternName: submitData.patternName,
+    })
+  }
+
+  return { getContentList, getContent, getContentPreview, getFormLatest, postFormAnswer, pageView, conversion }
 }
--- src/spec/composables/use-spearly.spec.ts
+++ src/spec/composables/use-spearly.spec.ts
@@ -1,12 +1,12 @@
-import { SpearlyApiClient } from '@spearly/sdk-js'
+import { SpearlyApiClient, SpearlyAnalytics } from '@spearly/sdk-js'
 import { useSpearly } from '../../composables'
 import { createContentListMock, createContentMock, createFormMock, createFormAnswerResponse } from '../createMock'
 
 describe('spearly composable', () => {
   const spearly = useSpearly()
 
-  it('Should return 5 functions', () => {
-    expect(Object.keys(spearly).length).toBe(5)
+  it('Should return 7 functions', () => {
+    expect(Object.keys(spearly).length).toBe(7)
   })
 
   describe('getContentList', () => {
@@ -95,9 +95,11 @@ describe('spearly composable', () => {
   })
 
   describe('postFormAnswer', () => {
+    const fields = { name: 'name', _spearly_gotcha: '' }
     let spyPostFormAnswer: jest.SpyInstance
 
     beforeEach(() => {
+      jest.spyOn(SpearlyApiClient.prototype, 'getFormLatest').mockResolvedValue(createFormMock())
       spyPostFormAnswer = jest
         .spyOn(SpearlyApiClient.prototype, 'postFormAnswers')
         .mockResolvedValue(createFormAnswerResponse())
@@ -108,8 +110,89 @@ describe('spearly composable', () => {
     })
 
     it('Should be return form answered', async () => {
-      const res = await spearly.postFormAnswer(1, { name: 'name', _spearly_gotcha: '' })
+      const res = await spearly.postFormAnswer({ name: 'name', _spearly_gotcha: '' }, 1)
       expect(res).toEqual(createFormAnswerResponse())
+    })
+
+    it('Version should be able to send form using response if not specified', async () => {
+      await spearly.getFormLatest('FORM_ID')
+      await spearly.postFormAnswer(fields)
+      expect(spyPostFormAnswer).toHaveBeenCalledWith(1, fields)
+    })
+
+    it('If you specify a version, should be submit the form with that version', async () => {
+      await spearly.postFormAnswer(fields, 15)
+      expect(spyPostFormAnswer).toHaveBeenCalledWith(15, fields)
+    })
+
+    it('Should throw an error with no version id', () => {
+      const newSpearly = useSpearly()
+      expect(() => newSpearly.postFormAnswer(fields)).rejects.toThrowError(new Error('version ID is not specified'))
+    })
+  })
+
+  describe('Analytics', () => {
+    describe('PageView', () => {
+      let spyPageView: jest.SpyInstance
+
+      beforeEach(() => {
+        spyPageView = jest.spyOn(SpearlyAnalytics.prototype, 'pageView').mockResolvedValue()
+        jest.spyOn(SpearlyApiClient.prototype, 'getContent').mockResolvedValue(createContentMock())
+      })
+
+      afterEach(() => {
+        jest.clearAllMocks()
+      })
+
+      it('Pageview should send the specified params', async () => {
+        await spearly.pageView('SPECIFIED_CONTENT_ID', 'b')
+        expect(spyPageView).toHaveBeenCalledWith({ patternName: 'b', contentId: 'SPECIFIED_CONTENT_ID' })
+      })
+
+      it('If params is not specified, ID and patternName obtained by getContent should be used', async () => {
+        await spearly.getContent('CONTENT_ID')
+        await spearly.pageView()
+        expect(spyPageView).toHaveBeenCalledWith({ patternName: 'b', contentId: '1' })
+      })
+
+      it('if "contentId" is not in either, it should be an error', async () => {
+        const newSpearly = useSpearly()
+        expect(() => newSpearly.pageView()).rejects.toThrowError(new Error('contentId is not defined.'))
+      })
+
+      it('if "patternName" is not in either, it should be an error', () => {
+        const newSpearly = useSpearly()
+        expect(() => newSpearly.pageView('CONTENT_ID')).rejects.toThrowError(new Error('patternName is not defined.'))
+      })
+    })
+  })
+
+  describe('Conversion', () => {
+    let spyConversion: jest.SpyInstance
+
+    beforeEach(() => {
+      spyConversion = jest.spyOn(SpearlyAnalytics.prototype, 'conversion').mockResolvedValue()
+    })
+
+    it('Pageview should send the specified params', async () => {
+      await spearly.conversion('SPECIFIED_CONTENT_ID', 'b')
+      expect(spyConversion).toHaveBeenCalledWith({ patternName: 'b', contentId: 'SPECIFIED_CONTENT_ID' })
+    })
+
+    it('If params is not specified, ID and patternName obtained by getContent should be used', async () => {
+      await spearly.getContent('CONTENT_ID')
+      await spearly.conversion()
+      expect(spyConversion).toHaveBeenCalledWith({ patternName: 'b', contentId: '1' })
+    })
+
+    it('if "contentId" is not in either, it should be an error', async () => {
+      const newSpearly = useSpearly()
+      expect(() => newSpearly.conversion()).rejects.toThrowError(new Error('contentId is not defined.'))
+    })
+
+    it('if "patternName" is not in either, it should be an error', () => {
+      const newSpearly = useSpearly()
+      expect(() => newSpearly.conversion('CONTENT_ID')).rejects.toThrowError(new Error('patternName is not defined.'))
     })
   })
 })
